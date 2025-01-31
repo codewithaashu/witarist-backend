a8b2f97e2ce9b535f18ae4ad5cc919c4ebb61a99
codewithaashu <codewithaashu1@gmail.com>
2025-01-31 12:02:28 +0530

    comments

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    //connect db
     const { connection } = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected at ${connection.port} port successfully.`);
   } catch (err) {
